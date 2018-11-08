22ae0ab454923406ebaca76dff1330d186b443bf
mdeutsch193 <mdeutsch@colby.edu>
2018-11-07 19:44:42 -0500

    Add files via upload

---
+++ NontrivialBracesCounter.java
@@ -0,0 +1,36 @@
+/*
+* File: proj8AbramsDeutschDurstJones.NontrivialBracesCounter.java
+* Names: Douglas Abrams, Martin Deutsch, Robert Durst, Matt Jones
+* Class: CS 361
+* Project 8
+* Date: November 9, 2018
+*/
+
+package proj8AbramsDeutschDurstJones;
+
+import java.io.*;
+
+/**
+ * This class contains a method for counting non-trivial left braces
+ *
+ * @author Douglas Abrams
+ * @author Martin Deutsch
+ * @author Robert Durst
+ * @author Matt Jones
+ */
+public class NontrivialBracesCounter {
+
+	/**
+	 * This method counts the number of non-trivial left braces
+	 * in the given Java file
+	 * @param fileName the name of the Java file
+	 * @return the number of non-trivial left braces in the file
+	 */
+	 public int getNumNontrivialLeftBraces(String fileName) {
+	 	return 0;
+	 }
+	 
+	 public static void main(String[] args) {
+	 	System.out.println("It works!");
+	 }
+}
