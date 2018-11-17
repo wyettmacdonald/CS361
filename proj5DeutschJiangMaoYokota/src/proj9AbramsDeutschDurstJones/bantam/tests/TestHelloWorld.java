75403997fb350940bfd21faa304c6258fb4a53b6
robertDurst <rsdurst@colby.edu>
2018-11-17 14:33:37 -0500

    Update makefile so that it compiles compiler and test suite and then add test suite make test command

---
+++ proj5DeutschJiangMaoYokota/src/proj9AbramsDeutschDurstJones/bantam/tests/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package proj9AbramsDeutschDurstJones.bantam.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+    @Test
+    public void isGteTest() {
+        assertTrue("One is greater than Zero", 1>0);
+    }
+}
+
